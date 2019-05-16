1bf0cc6f0a44dc8c6680723b6d1ca3b846b14b3b
Joseph Montanez <sutabi@gmail.com>
2019-05-16 01:17:13 -0700

    not a great fix...
    This is a deeply nested issue that is an issue with Sximo.

--- app/Library/SximoHelpers.php
+++ app/Library/SximoHelpers.php
@@ -196,9 +196,24 @@ class SximoHelpers {
      if(count($sql)>=1)
      {
         $row = $sql[0];
+
+        $mapped_module_name = ucwords($row->module_name);
+        $mapped_module_names = [
+                'UserTrip' => ['Usertrips'],
+        ];
+
+        foreach ($mapped_module_names as $model_name => $matches) {
+            foreach ($matches as $match) {
+                if ($mapped_module_name === $match) {
+                    $mapped_module_name = $model_name;
+                }
+            }
+        }
+
         // Load Model
-        $model = '\\App\\Models\\'.ucwords($row->module_name);
-        $model = new $model();
+        $model_class_path = '\\App\\Models\\' . $mapped_module_name;
+
+        $model = new $model_class_path();
         $info = $model->makeInfo( $row->module_id);
         $data['row'] =  $model->find(0);
 
