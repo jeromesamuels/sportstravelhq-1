2573f81b6bce886b62ae7a0d488cac87731679ed
Joseph Montanez <sutabi@gmail.com>
2019-05-15 20:02:59 -0700

    Allow trip status logs to be deleted from the model.

--- app/Models/Tripstatuslogs.php
+++ app/Models/Tripstatuslogs.php
@@ -1,12 +1,14 @@
 <?php namespace App\Models;
 
 use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class tripstatuslogs extends Sximo  {
 	
 	protected $table = 'trip_status_logs';
-	protected $primaryKey = '';
+	protected $primaryKey = ['trip_id', 'user_id', 'rfp_id'];
+    public $incrementing = false;
 
 	public function __construct() {
 		parent::__construct();
@@ -26,6 +28,43 @@ class tripstatuslogs extends Sximo  {
 	public static function queryGroup(){
 		return "  ";
 	}
-	
 
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        $keys = $this->getKeyName();
+        if(!is_array($keys)){
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        foreach($keys as $keyName){
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    /**
+     * Get the primary key value for a save query.
+     *
+     * @param mixed $keyName
+     * @return mixed
+     */
+    protected function getKeyForSaveQuery($keyName = null)
+    {
+        if(is_null($keyName)){
+            $keyName = $this->getKeyName();
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
+    }
 }
